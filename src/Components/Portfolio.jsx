1f715a57e5cd538ff1582e73d8488ec7ec89d41d
kelechi chibundu <kceechibundu@gmail.com>
2023-10-31 13:43:32 +0000

    updates to my portfolio

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -31,28 +31,27 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "CALCULATOR APP",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "a simple easy to use calculator that is beginner friendly and aids faster calculation ",
+    url: "https://github.com/Kaycee9/myCalculatorApp.git",
   },
   {
-    title: "Web Development for Beginners",
+    title: "STARBUZZ COFFEE",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "A website for ordering coffee to be delivered at ease to your doorstep.",
+    url: "https://github.com/Kaycee9/starbuzzCoffee.git",
   },
   {
-    title: "My Resume Site",
+    title: "A PHOTOGRAPHY WEBSITE",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "A website where creators (photographers) showcase their work",
+    url: "https://github.com/Kaycee9/photographyWebsite.git",
   },
   {
-    title: "GitHub Codespaces and github.dev",
-    description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+    title: "DONUT HUB",
+    description:"An E-site for ordering Donuts",
+    url: "https://github.com/Kaycee9/donutRepo.git",
   },
 ];
 
